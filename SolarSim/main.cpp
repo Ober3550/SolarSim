486ca72cfdf062f777736b93cc9f80c59030aae0
Ober3550 <41331378+Ober3550@users.noreply.github.com>
2020-08-05 09:55:17 +1000

    Added planet merging

--- SolarSim/main.cpp
+++ SolarSim/main.cpp
@@ -11,6 +11,7 @@
 namespace fs = std::experimental::filesystem;
 
 sf::Texture circle;
+double pi = 2 * acos(0.0);
 
 struct Planet {
     int* id;
@@ -58,13 +59,6 @@ public:
         group->mass[index] = mass;
         planetsLength++;
     }
-    SolarSystem()
-    {
-        AddPlanet(-200, 0, 0, -1, 0.5);
-        AddPlanet(200, 0,  0, 1, 0.5);
-        AddPlanet(600, 0,  0, 1, 0.05);
-        AddPlanet(-600, 0,  0, -1, 0.05);
-    }
     PlanetOption GetPlanet(int id)
     {
         id--;
@@ -86,6 +80,39 @@ public:
         }
         return PlanetOption();
     }
+    void RemovePlanet(int id)
+    {
+        id--;
+        if (id / groupSize < planets.size())
+        {
+            PlanetGroup* group = &planets[id / groupSize];
+            int index = id % groupSize;
+            if (PlanetOption temp = GetPlanet(planetsLength))
+            {
+                Planet lastPlanet = temp;
+                group->x[index]     = *lastPlanet.x;
+                group->y[index]     = *lastPlanet.y;
+                group->dx[index]    = *lastPlanet.dx;
+                group->dy[index]    = *lastPlanet.dy;
+                group->mass[index]  = *lastPlanet.mass;
+                planetsLength--;
+            }
+        }
+    }
+    void AddRandomSatellite(int id) {
+        if (PlanetOption temp = GetPlanet(id))
+        {
+            Planet parent = temp;
+
+        }
+    }
+    SolarSystem()
+    {
+        AddPlanet(-200, 0, 0, -1, 0.5);
+        AddPlanet(200, 0, 0, 1, 0.5);
+        AddPlanet(600, 0, 0, 1, 0.05);
+        AddPlanet(-600, 0, 0, -1, 0.05);
+    }
     std::vector<sf::Sprite> DrawSolarSystem()
     {
         std::vector<sf::Sprite> sprites;
@@ -98,7 +125,8 @@ public:
                 sprite.setTexture(circle);
                 sprite.setOrigin(128, 128);
                 sprite.setPosition(*planet.x, *planet.y);
-                sprite.setScale(*planet.mass, *planet.mass);
+                float r = sqrt(*planet.mass / pi);
+                sprite.setScale(r, r);
                 sprites.emplace_back(sprite);
             }
         }
@@ -128,6 +156,20 @@ public:
                             float rx = ((*planetB.x) - (*planetA.x));
                             float ry = ((*planetB.y) - (*planetA.y));
                             float r2 = pow(rx,2) + pow(ry,2);
+                            if (r2 < 100)
+                            {
+                                float relativeMass = (*planetB.mass / *planetA.mass);
+                                *planetA.x += rx * relativeMass;
+                                *planetA.y += ry * relativeMass;
+                                *planetA.dx = (*planetA.dx) * (*planetA.mass) + (*planetB.dx) * (*planetB.mass);
+                                *planetA.dy = (*planetA.dy) * (*planetA.mass) + (*planetB.dy) * (*planetB.mass);
+                                *planetA.mass += *planetB.mass;
+                                // Remove planet B
+                                // Recalculate planet As forces since its mass changed
+                                RemovePlanet(j);
+                                i--;
+                                goto skip_calc;
+                            }
                             float F = (G * (*planetA.mass) * (*planetB.mass)) / r2;
                             Fx += F * rx;
                             Fy += F * ry;
@@ -138,6 +180,8 @@ public:
                 *planetA.dy += Fy / (*planetA.mass);
                 *planetA.x += *planetA.dx;
                 *planetA.y += *planetA.dy;
+
+            skip_calc: continue;
             }
         }
     }
@@ -159,6 +203,9 @@ public:
 
 int main()
 {
+    sf::Clock deltaClock;
+    sf::Clock frameClock;
+
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "ImGui + SFML = <3");
     window.setFramerateLimit(60);
     ImGui::SFML::Init(window);
@@ -169,22 +216,19 @@ int main()
         std::cout << entry.path() << '\n';
     }*/
 
-    circle.loadFromFile("circle.png");
-
-    SolarSystem system;
-
     sf::View centreView;
     sf::Vector2u size = window.getSize();
     centreView.setSize(sf::Vector2f(size.x, size.y));
     centreView.setCenter(0, 0);
+    float prevZoom = 1.f;
+    float zoom = 1.f;
 
+    circle.loadFromFile("circle.png");
     int selectedPlanet = 1;
 
-    float prevZoom  = 1.f;
-    float zoom      = 1.f;
+    srand(std::hash<int>{}(frameClock.getElapsedTime().asMicroseconds()));
+    SolarSystem system;
 
-    sf::Clock deltaClock;
-    sf::Clock frameClock;
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -223,7 +267,11 @@ int main()
         ImGui::SFML::Update(window, deltaClock.restart());
 
         ImGui::Begin("Update Rate");
-        ImGui::Text(std::string("FPS: "+std::to_string(1000000/frameClock.getElapsedTime().asMicroseconds())).c_str());
+        int frameRate = 1000000 / frameClock.getElapsedTime().asMicroseconds();
+        static int updatesPerFrame = 1;
+        ImGui::Text(std::string("FPS: "+std::to_string(frameRate)).c_str());
+        ImGui::Text(std::string("UPS: " + std::to_string(frameRate * updatesPerFrame)).c_str());
+        ImGui::SliderInt(" :Updates Per Frame", &updatesPerFrame, 1, 10);
         ImGui::End();
         frameClock.restart();
 
@@ -248,13 +296,16 @@ int main()
             }
         }
         if (!ImGui::IsWindowFocused())
-            system.UpdatePlanets();
+        {
+            for (int i = 0; i < updatesPerFrame; i++)
+            {
+                system.UpdatePlanets();
+            }
+        }
 
         ImGui::End();
 
         //ImGui::ShowTestWindow();
-
-        
 
         std::vector<sf::Sprite> sprites = system.DrawSolarSystem();
         for (int i=0;i<sprites.size();i++)
